e2952b7b191f725829b5af430cfe893ffd9090a8
Kshitij Dutt <kshtjdutt07@gmail.com>
2021-08-08 18:22:17 +0530

    final commit

--- server/controller/users.js
+++ server/controller/users.js
@@ -68,12 +68,3 @@ export const signup = async (req, res) => {
     console.log(error);
   }
 };
-
-export const test = async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(404).json({ message: error });
-  }
-};
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,10 +2,9 @@ import express from "express";
 
 const router = express.Router();
 
-import { signin, signup,test } from "../controller/users.js";
+import { signin, signup, test } from "../controller/users.js";
 
 // we will be using the post request for signin and signup controllers
-router.get("/");
 router.post("/signin", signin);
 router.post("/signup", signup);
 
